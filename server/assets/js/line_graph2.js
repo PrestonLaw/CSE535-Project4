860350316455657a1774f03df0fba86bc94e644b
Preston Law <preston.law@gmail.com>
2018-12-06 23:26:58 -0500

    I added a D3 line graph that I plan to use as a tweet volume over time display.

---
+++ server/assets/js/line_graph2.js
@@ -0,0 +1,96 @@
+
+
+
+
+var search_for_tweets = function(search_str, callback) {
+    search_str = search_str.replace(" ", "%20");
+
+    //var day = 737011;
+    //var search_url = 'search/solr/p4/select?fq=date_ordinal:' + day + '&q=' + searchstring + '&wt=json';
+
+    var num = 10000
+    var search_url = 'search/solr/p4/select?q='+search_str+'&rows='+num+'&start=0&wt=json'
+    
+    d3.json(search_url, function (resp) {
+        var tweets = resp.response.docs;
+	(callback)(tweets);
+    })
+};
+
+
+
+
+var line_graph = function() {
+
+    // Set the dimensions of the canvas / graph
+    var margin = {top: 30, right: 20, bottom: 30, left: 50},
+	width = 600 - margin.left - margin.right,
+	height = 270 - margin.top - margin.bottom;
+
+    // Parse the date / time
+    var parseDate = d3.timeParse("%b %Y");
+
+    // Set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // Define the line
+    var priceline = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.price); });
+
+    // Adds the svg canvas
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+	      "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    d3.csv("assets/data/line_graph2.csv", function(error, data) {
+	if (error) throw error;
+
+	data.forEach(function(d) {
+	    d.date = parseDate(d.date);
+	    d.price = +d.price;
+	});
+
+	// Scale the range of the data
+	x.domain(d3.extent(data, function(d) { return d.date; }));
+	y.domain([0, d3.max(data, function(d) { return d.price; })]);
+
+	// Nest the entries by symbol
+	var dataNest = d3.nest()
+	    .key(function(d) {return d.symbol;})
+	    .entries(data);
+
+	var color = d3.scaleOrdinal(d3.schemeCategory10);  // set the colour scale
+
+	// Loop through each symbol / key
+	dataNest.forEach(function(d) {
+
+	    svg.append("path")
+	        .attr("class", "line")
+	        .style("stroke", function() { // Add dynamically
+		    return d.color = color(d.key); })
+	        .attr("d", priceline(d.values));
+
+	});
+
+	// Add the X Axis
+	svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(d3.axisBottom(x));
+
+	// Add the Y Axis
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .call(d3.axisLeft(y));
+
+    });
+
+
+}

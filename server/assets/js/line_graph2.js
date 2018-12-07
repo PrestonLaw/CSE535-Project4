356fc24887ed7e61addc6c1651fb8a407d3a036c
Preston Law <preston.law@gmail.com>
2018-12-07 01:51:52 -0500

    I got the topic tweet volume graph over time working with real data.

--- server/assets/js/line_graph2.js
+++ server/assets/js/line_graph2.js
@@ -28,16 +28,16 @@ var line_graph = function() {
 	height = 270 - margin.top - margin.bottom;
 
     // Parse the date / time
-    var parseDate = d3.timeParse("%b %Y");
+    var parseDate = d3.timeParse("%Y-%m-%d");
 
     // Set the ranges
     var x = d3.scaleTime().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
 
     // Define the line
-    var priceline = d3.line()
+    var countline = d3.line()
         .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.price); });
+        .y(function(d) { return y(d.count); });
 
     // Adds the svg canvas
     var svg = d3.select("body")
@@ -49,33 +49,33 @@ var line_graph = function() {
 	      "translate(" + margin.left + "," + margin.top + ")");
 
     // Get the data
-    d3.csv("assets/data/line_graph2.csv", function(error, data) {
+    d3.csv("assets/data/tweet_date_count.csv", function(error, data) {
 	if (error) throw error;
 
 	data.forEach(function(d) {
 	    d.date = parseDate(d.date);
-	    d.price = +d.price;
+	    d.count = +d.count;
 	});
 
 	// Scale the range of the data
 	x.domain(d3.extent(data, function(d) { return d.date; }));
-	y.domain([0, d3.max(data, function(d) { return d.price; })]);
+	y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
 	// Nest the entries by symbol
 	var dataNest = d3.nest()
-	    .key(function(d) {return d.symbol;})
+	    .key(function(d) {return d.topic;})
 	    .entries(data);
 
 	var color = d3.scaleOrdinal(d3.schemeCategory10);  // set the colour scale
 
 	// Loop through each symbol / key
 	dataNest.forEach(function(d) {
-
+	    console.log(d.key)
 	    svg.append("path")
 	        .attr("class", "line")
 	        .style("stroke", function() { // Add dynamically
 		    return d.color = color(d.key); })
-	        .attr("d", priceline(d.values));
+	        .attr("d", countline(d.values));
 
 	});
 
